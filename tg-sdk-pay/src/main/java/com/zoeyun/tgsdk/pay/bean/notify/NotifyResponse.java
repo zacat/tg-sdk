9ec1a5799337d374d4bfe6b170715e3934a00915
dong-c <271139015@qq.com>
2022-10-13 18:24:35 +0800

    增加回调Response值

---
+++ tg-sdk-pay/src/main/java/com/zoeyun/tgsdk/pay/bean/notify/NotifyResponse.java
@@ -0,0 +1,8 @@
+package com.zoeyun.tgsdk.pay.bean.notify;
+
+public class NotifyResponse {
+
+    public static final transient String FAIL = "FAIL";
+
+    public static final transient String SUCCESS = "SUCCESS";
+}
--- tg-sdk-pay/src/main/java/com/zoeyun/tgsdk/utils/Md5Utils.java
+++ tg-sdk-pay/src/main/java/com/zoeyun/tgsdk/utils/Md5Utils.java
@@ -37,17 +37,5 @@ public class Md5Utils {
         return null;
     }
 
-    private static String byte2Hex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : bytes) {
-            String hex = Integer.toHexString(b & 0xFF);
-            if (hex.length() == 1) {
-                hex = "0" + hex;   // one byte to double-digit hex
-            }
-            sb.append(hex);
-        }
-        return sb.toString();
-    }
-
 
 }
