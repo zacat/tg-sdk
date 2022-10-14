0c3398308128f2798df42515101ec5b1dd3fca32
dong-c <271139015@qq.com>
2022-10-14 14:44:34 +0800

    修改参数对象

--- tg-sdk-pay/src/main/java/com/zoeyun/tgsdk/pay/config/PayConfig.java
+++ tg-sdk-pay/src/main/java/com/zoeyun/tgsdk/pay/config/PayConfig.java
@@ -3,14 +3,19 @@ package com.zoeyun.tgsdk.pay.config;
 public interface PayConfig {
 
     String getMchId();
+    void setMchId(String mchId);
 
     String getMchKey();
+    void setMchKey(String mchKey);
 
     String getPayBaseUrl();
+    void setPayBaseUrl(String payBaseUrl);
+
 
     int getHttpConnectionTimeout();
+    void setHttpConnectionTimeout(int httpConnectionTimeout);
 
     int getHttpTimeout();
+    void setHttpTimeout(int httpTimeout);
 
-    Boolean getUseSandboxEnv();
 }
--- tg-sdk-pay/src/main/java/com/zoeyun/tgsdk/pay/config/impl/PayConfigImpl.java
+++ tg-sdk-pay/src/main/java/com/zoeyun/tgsdk/pay/config/impl/PayConfigImpl.java
@@ -10,26 +10,12 @@ public class PayConfigImpl implements PayConfig, Serializable {
 
     private static final long serialVersionUID = -5234628670973234280L;
 
-    private static final String DEFAULT_PAY_BASE_URL = "https://tgpay.833006.net/";
-    private static final String DEFAULT_SANDBOX_PAY_BASE_URL = "https://tgpay.833006.net";
     protected volatile String mchId;
     protected volatile String mchKey;
+    protected volatile String payBaseUrl;
     protected volatile int httpConnectionTimeout = 5000;
     private int httpTimeout = 10000;
     private boolean useSandboxEnv = false;
-
-    @Override
-    public Boolean getUseSandboxEnv() {
-        return useSandboxEnv;
-    }
-
-    @Override
-    public String getPayBaseUrl() {
-        if (useSandboxEnv) {
-            return DEFAULT_SANDBOX_PAY_BASE_URL;
-        }
-        return DEFAULT_PAY_BASE_URL;
-    }
 
 
 }
